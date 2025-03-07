f189b436628f229872c2e18af92672b173594933
Anfal <anfal.korichi@ensia.edu.dz>
2025-03-07 16:27:26 +0100

    Users Table

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,12 +1,24 @@
 import * as React from "react";
 import Box from "@mui/joy/Box";
 import Sidebar from "./Sidebar"; 
+import Header from "./Header"
+import UserTable from "./UserTable"
+import { Typography } from "@mui/joy";
 
 export default function Dashboard() {
   return (
-    <Box sx={{ display: "flex", minHeight: "100vh" }}>
+    <Box sx={{ display: "flex", minHeight: "100vh"}}>
       <Sidebar />
-      <p>Users page</p>
+      <Box sx={{ flexGrow: 1, p: 3 }}>
+        <Typography level="h1" sx={{ fontSize: "2rem", fontWeight: "bold", mb: 2 }}>
+          Users
+        </Typography>
+
+        {/* User Table */}
+        <UserTable />
+      </Box>
+      
+
     </Box>
   );
 }
