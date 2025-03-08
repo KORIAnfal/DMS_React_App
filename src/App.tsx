79d98618595a5e612c71cb6a665bd56cada4bfab
Anfal <anfal.korichi@ensia.edu.dz>
2025-03-08 17:36:54 +0100

    Redux state managment-fixed

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 import { useSelector } from "react-redux";
 import { RootState } from "./store/store";
 import { JSX } from "react";
+import { CssVarsProvider } from "@mui/joy";
 
 function ProtectedRoute({ children }: { children: JSX.Element }) {
   const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
@@ -13,11 +14,13 @@ function ProtectedRoute({ children }: { children: JSX.Element }) {
 
 function App() {
   return (
+    <CssVarsProvider>
     <Routes>
       <Route path="/" element={<Login />} /> 
       <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
       <Route path="/users" element={<ProtectedRoute><Users /></ProtectedRoute>} />
     </Routes>
+    </CssVarsProvider>
   );
 }
 
