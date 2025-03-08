4cf617350bac36115dfc3026971ee4763c5e43b4
Anfal <anfal.korichi@ensia.edu.dz>
2025-03-08 01:50:45 +0100

    Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Users from "./components/Users"; 
 import Dashboard from "./components/Dashboard"; 
-
+import Login from "./components/Login"; 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/users" element={<Users />} />
       </Routes>
